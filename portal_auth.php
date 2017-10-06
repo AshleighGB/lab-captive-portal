8357c5f1e768bc5fcbf5b20922a5ee1ddb8a12f5
Wareing <Wareing@users.noreply.github.com>
2017-10-06 20:07:39 +0100

    Create portal_auth.php

---
+++ portal_auth.php
@@ -0,0 +1,71 @@
+<html>
+<body>
+<form method="post" action="https://network-login.trinity-cloud.com:8005/index.php?zone=accessweb">
+	<input name="redirurl" type="hidden" value="https://joshuawareing.me">
+	<input name="zone" type="hidden" value="accessweb">
+	<center>
+	<table cellpadding="6" cellspacing="0" width="550" height="380" style="border:1px solid #000000">
+	<tr height="10" style="background-color:#990000">
+		<td style="border-bottom:1px solid #000000">
+			<font color='white'>
+			<b>
+				pfSense captive portal
+			</b>
+			</font>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			<div id="mainlevel">
+			<center>
+			<table width="100%" border="0" cellpadding="5" cellspacing="0">
+			<tr>
+				<td>
+					<center>
+					<div id="mainarea">
+					<center>
+					<table width="100%" border="0" cellpadding="5" cellspacing="5">
+					<tr>
+						<td>
+							<div id="maindivarea">
+							<center>
+								<div id='statusbox'>
+									<font color='red' face='arial' size='+1'>
+									<b>
+										
+									</b>
+									</font>
+								</div>
+								<br />
+								<div id='loginbox'>
+								<table>
+									<tr><td colspan="2"><center>Welcome to the pfSense Captive Portal!</td></tr>
+									<tr><td>&nbsp;</td></tr>
+									<tr><td class="text-right">Username:</td><td><input name="auth_user" type="text" style="border: 1px dashed;"></td></tr>
+									<tr><td class="text-right">Password:</td><td><input name="auth_pass" type="password" style="border: 1px dashed;"></td></tr>
+									<tr><td>&nbsp;</td></tr>
+									<tr>
+										<td colspan="2"><center><input name="accept" type="submit" value="Continue"></center></td>
+									</tr>
+								</table>
+								</div>
+							</center>
+							</div>
+						</td>
+					</tr>
+					</table>
+					</center>
+					</div>
+					</center>
+				</td>
+			</tr>
+			</table>
+			</center>
+			</div>
+		</td>
+	</tr>
+	</table>
+	</center>
+</form>
+</body>
+</html>
